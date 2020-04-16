3429f9a0148e03b9bce826d69f55ea7cd1e6e092
PixelRupi <piotrekb.345@gmail.com>
2020-04-16 12:03:37 +0200

    Add files via upload

---
+++ BINON2/BINON2/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BINON2
+{
+    public partial class BINON : Form
+    {
+        public BINON()
+        {
+            InitializeComponent();
+        }
+
+        private void BINON_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int licznik = 1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button7.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button9.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button10.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            button11.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            button13.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            button14.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            button15.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            button16.Enabled = false;
+            textBox1.Text = licznik.ToString();
+            licznik++;
+        }
+    }
+}
